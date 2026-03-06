9d54d341dc3ea4e765fcc45ea691acccb350dc17
azizghannamm-tech <azizghannamm@gmail.com>
2026-03-06 11:20:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
 getAuth,
 signInWithEmailAndPassword,
+createUserWithEmailAndPassword,
 signInWithPopup,
 GoogleAuthProvider,
 onAuthStateChanged,
@@ -13,30 +14,53 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyB8dDTnpPQVRAs7dkfc8QU3L5qUJtm-2jg",
-authDomain: "affiliate-relations-17687.firebaseapp.com",
-projectId: "affiliate-relations-17687",
-appId: "1:642027131905:web:5f0076ee7b34578b9f9c00"
+apiKey:"AIzaSyB8dDTnpPQVRAs7dkfc8QU3L5qUJtm-2jg",
+authDomain:"affiliate-relations-17687.firebaseapp.com",
+projectId:"affiliate-relations-17687",
+appId:"1:642027131905:web:5f0076ee7b34578b9f9c00"
 
 };
 
+const app=initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const provider=new GoogleAuthProvider();
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
 
-
-window.login = function(){
+window.login=function(){
 
 const email=document.getElementById("email").value;
 const password=document.getElementById("password").value;
 
 signInWithEmailAndPassword(auth,email,password)
 .then(()=>{
+
 window.location="index.html";
+
 })
 .catch(err=>{
+
 document.getElementById("error").innerText=err.message;
+
+});
+
+};
+
+
+window.signup=function(){
+
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
+
+createUserWithEmailAndPassword(auth,email,password)
+.then(()=>{
+
+window.location="index.html";
+
+})
+.catch(err=>{
+
+document.getElementById("error").innerText=err.message;
+
 });
 
 };
@@ -46,10 +70,14 @@ window.googleLogin=function(){
 
 signInWithPopup(auth,provider)
 .then(()=>{
+
 window.location="index.html";
+
 })
 .catch(err=>{
+
 document.getElementById("error").innerText=err.message;
+
 });
 
 };
