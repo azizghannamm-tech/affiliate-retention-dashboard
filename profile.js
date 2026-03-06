b5ad9338efedfed7579d76d1bb6b22f43e1a1a50
azizghannamm-tech <azizghannamm@gmail.com>
2026-03-06 13:57:28 -0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -2,6 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+
+/* FIREBASE */
+
 const firebaseConfig = {
 apiKey: "AIzaSyB8dDTnpPQVRAs7dkfc8QU3L5qUJtm-2jg",
 authDomain: "affiliate-relations-17687.firebaseapp.com",
@@ -12,17 +15,21 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+
+/* ELEMENTS */
+
+const profileAvatar = document.getElementById("profileAvatar");
+const avatarUpload = document.getElementById("avatarUpload");
+
 const nameInput = document.getElementById("profileName");
 const bioInput = document.getElementById("profileBio");
 const saveBtn = document.getElementById("saveProfile");
 
-const avatarUpload = document.getElementById("avatarUpload");
-const profileAvatar = document.getElementById("profileAvatar");
-
 let currentUser;
-let avatarData = "";
+let avatarBase64 = "";
 
-/* LOAD PROFILE */
+
+/* AUTH */
 
 onAuthStateChanged(auth, async (user) => {
 
@@ -40,9 +47,9 @@ const data = snap.data();
 nameInput.value = data.name || "";
 bioInput.value = data.bio || "";
 
-if(data.avatar){
-profileAvatar.src = data.avatar;
-avatarData = data.avatar;
+if(data.photo){
+profileAvatar.src = data.photo;
+avatarBase64 = data.photo;
 }
 
 }
@@ -51,7 +58,8 @@ avatarData = data.avatar;
 
 });
 
-/* AVATAR PREVIEW */
+
+/* IMAGE UPLOAD */
 
 avatarUpload.addEventListener("change", function(){
 
@@ -64,15 +72,16 @@ const reader = new FileReader();
 reader.onload = function(e){
 
 profileAvatar.src = e.target.result;
-avatarData = e.target.result;
+avatarBase64 = e.target.result;
 
-}
+};
 
 reader.readAsDataURL(file);
 
 }
 
 });
+
 
 /* SAVE PROFILE */
 
@@ -82,7 +91,7 @@ await setDoc(doc(db,"profiles",currentUser.uid),{
 
 name: nameInput.value,
 bio: bioInput.value,
-avatar: avatarData
+photo: avatarBase64
 
 });
 
