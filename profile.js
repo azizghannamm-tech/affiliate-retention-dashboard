e52b1e65703392a1a9468de2e057c6b191593907
azizghannamm-tech <azizghannamm@gmail.com>
2026-03-06 13:20:07 -0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -16,7 +16,13 @@ const nameInput = document.getElementById("profileName");
 const bioInput = document.getElementById("profileBio");
 const saveBtn = document.getElementById("saveProfile");
 
+const avatarUpload = document.getElementById("avatarUpload");
+const profileAvatar = document.getElementById("profileAvatar");
+
 let currentUser;
+let avatarData = "";
+
+/* LOAD PROFILE */
 
 onAuthStateChanged(auth, async (user) => {
 
@@ -34,18 +40,49 @@ const data = snap.data();
 nameInput.value = data.name || "";
 bioInput.value = data.bio || "";
 
+if(data.avatar){
+profileAvatar.src = data.avatar;
+avatarData = data.avatar;
+}
+
 }
 
 }
 
 });
 
+/* AVATAR PREVIEW */
+
+avatarUpload.addEventListener("change", function(){
+
+const file = this.files[0];
+
+if(file){
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+profileAvatar.src = e.target.result;
+avatarData = e.target.result;
+
+}
+
+reader.readAsDataURL(file);
+
+}
+
+});
+
+/* SAVE PROFILE */
+
 saveBtn.onclick = async () => {
 
 await setDoc(doc(db,"profiles",currentUser.uid),{
 
-name:nameInput.value,
-bio:bioInput.value
+name: nameInput.value,
+bio: bioInput.value,
+avatar: avatarData
 
 });
 
