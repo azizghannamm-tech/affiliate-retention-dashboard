74e85ff560ca4c1349d058dd571f779a73332af5
azizghannamm-tech <azizghannamm@gmail.com>
2026-03-09 09:00:02 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8dDTnpPQVRAs7dkfc8QU3L5qUJtm-2jg",
+  authDomain: "affiliate-relations-17687.firebaseapp.com",
+  projectId: "affiliate-relations-17687",
+  storageBucket: "Yaffiliate-relations-17687.firebasestorage.app",
+  messagingSenderId: "642027131905",
+  appId: "1:642027131905:web:5f0076ee7b34578b9f9c00"
+};
+
+export const app = initializeApp(firebaseConfig);
