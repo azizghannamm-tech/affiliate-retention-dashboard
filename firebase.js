a3c2ac9df4c8de735d8c855dfed1ebccc7f56b66
azizghannamm-tech <azizghannamm@gmail.com>
2026-05-14 10:14:01 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,38 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getAuth,
+GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8dDTnpPQVRAs7dkfc8QU3L5qUJtm-2jg",
-  authDomain: "affiliate-relations-17687.firebaseapp.com",
-  projectId: "affiliate-relations-17687",
-  storageBucket: "Yaffiliate-relations-17687.firebasestorage.app",
-  messagingSenderId: "642027131905",
-  appId: "1:642027131905:web:5f0076ee7b34578b9f9c00"
+apiKey: "AIzaSyB8dDTnpPQVRAs7dkfc8QU3L5qUJtm-2jg",
+authDomain: "affiliate-relations-17687.firebaseapp.com",
+projectId: "affiliate-relations-17687",
+storageBucket: "affiliate-relations-17687.appspot.com",
+messagingSenderId: "642027131905",
+appId: "1:642027131905:web:5f0076ee7b34578b9f9c00"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const googleProvider = new GoogleAuthProvider();
+
+export {
+app,
+auth,
+db,
+storage,
+googleProvider
+};
